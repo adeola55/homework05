07c6428723ff681362126cdf7c93442e6642ad8b
adeola afariogun <kokobilo55@gmail.com>
2020-03-30 18:25:22 -0700

    modifing dayplanner

--- Develop/script.js
+++ Develop/script.js
@@ -17,21 +17,32 @@ $("#savebutton").on("click",function(event){
     localStorage.setItem("dayplanner",JSON.stringify(plannerentries))
     console.log(plannerentries);
     console.log(JSON.parse(localStorage.getItem("dayplanner")))
+    location.reload()
 });
 $("#clearbutton").on("click",function(event){
     event.preventDefault();
      localStorage.removeItem("dayplanner")
-     console.log("Local storage dayplanner", JSON.parse(localStorage.getItem("dayplanner")))
+     console.log("Local storage dayplanner", JSON.parse(localStorage.getItem("dayplanner")));
+     location.reload()
 })
 function getlocalstorage(){
     console.log("Local storage dayplanner", JSON.parse(localStorage.getItem("dayplanner")))
-    var currenttime=moment().format("HH")
+    var currenttime=parseInt(moment().format("HH"))
     console.log(currenttime)
     for(let i=0; i < plannerentries.length; i++){
-        var timeclock=plannerentries[i].time
+        var timeclock=parseInt(plannerentries[i].time)
         var userentry=plannerentries[i].value
+        console.log(timeclock)
         $(`#${timeclock}`).val(userentry)
-
+         if(currenttime < timeclock){
+             $(`#${timeclock}`).addClass("future")
+         } 
+         else if(currenttime===timeclock){
+            $(`#${timeclock}`).addClass("present")
+         }
+         else{
+            $(`#${timeclock}`).addClass("past")
+         }
     }
 }
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
